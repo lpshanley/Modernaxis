b67af53b6b2428bf36d28cbcb35f6151e234993b
Lucas Shanley <lpshanley@users.noreply.github.com>
2014-10-17 14:25:49 -0500

    Added navigate to page function
    Added navigate to page.
    Fixed Search function.

--- sitescripts/FUNCTIONS.js
+++ sitescripts/FUNCTIONS.js
@@ -7,8 +7,17 @@ function pageLoad() {
 
 //------------ Testing Area ------------------
 
+          //------- Test Data---
+          
+var case_number = 211486;
+var footer_month = 10;
+var footer_year = 14;
+
+          //--------------------
+
 function testFunction() {
-   alert("Test function region");
+  alert("Are you in training or production? Navigate to a panel in 211486(training case) this will bring you to STAT/MEMB.");
+  navigate_to_screen("STAT","MEMB");
 }
 
 function runme() {
@@ -103,10 +112,10 @@ function writeScreen(text,row,col) {
 	host.Focus();
 }
 
-function readScreen(var_name,len,row,col) {
-    var var_name = new Object();
-    host.ReadScreen (nar_name, len, row, col);
-	host.Focus();
+function readScreen(len, row, col) {
+    var X = new Object();
+    host.ReadScreen (X, len, row, col);
+    return(X.Str);
 }
 
 function sendKey(X) {
@@ -128,10 +137,27 @@ function getCursor() {
 }
 
 function searchScreen(X) {
-    row = 1;
-    col = 1;
-    Host.Search(X, row, col);
-    Return(row, col);  
+  var row = 1;
+  var runs = 1;
+  while (runs != 25) {
+    var rdln = readScreen(80, row, 1)
+    var rdln = rdln.search(X)
+    if(rdln != -1) {
+      var col = rdln + 1;
+      var runs = 25;
+    } else {    
+    var row = row + 1;
+    var runs = runs + 1;
+    }
+  }
+  if(row == 25) {
+    var row = 0;
+    var col = 0;
+  }
+  return {
+    row: row,
+    col: col
+  };
 }
 
 //---------- Basic Key Stroke Functions ----------------
@@ -211,5 +237,49 @@ function transmit() {
   host.WaitReady(0,0);
 }
 
-//------------------------------------------------------
+//-- Converted VB Functions -----------------------------
 
+function navigate_to_screen(X, Y) {
+  sendKey('@E');
+  waitReady(0,0);
+  var MAXIS_check = readScreen(5, 1, 39);
+  if(MAXIS_check == "MAXIS" || MAXIS_check == "AXIS ") {
+    var search = searchScreen("Function: ");
+    var row = search.row;
+    var col = search.col;
+    if(row != 0) { 
+      var MAXIS_function = readScreen(4, row, col + 10);
+      var STAT_note_check = readScreen(4, 2, 45);
+      var search = searchScreen("Case Nbr: ");
+      var row = search.row;
+      var col = search.col; 
+      var current_case_number = readScreen(8, row, col + 10);
+      current_case_number = current_case_number.replace("__", "");
+      current_case_number = current_case_number.trim();
+    }
+    if(current_case_number == case_number && MAXIS_function == X.toUpperCase() && STAT_note_check != "NOTE") { 
+      var search = searchScreen("Command: ");
+      var row = search.row;
+      var col = search.col;
+      writeScreen(Y, row, col + 9);
+      transmit();
+    }else {
+      var SELF_check = "";
+      while (SELF_check != "SELF") {
+        PF3();
+        var SELF_check = readScreen(4, 2, 50);
+      }
+      writeScreen(X, 16, 43);
+      writeScreen("________", 18, 43);
+      writeScreen(case_number, 18, 43);
+      writeScreen(footer_month, 20, 43);
+      writeScreen(footer_year, 20, 46);
+      writeScreen(Y, 21, 70);
+      transmit();
+      var abended_check = readScreen(7, 9, 27);
+      if(abended_check == "abended") {
+        transmit();
+      }
+    }
+  }
+}
